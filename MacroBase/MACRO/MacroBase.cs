69b39570d4ad4485c953c4618093e31347edfa79
nonnon <nonnon@mail.com>
2020-09-12 15:39:29 +0900

    プロジェクト ファイルを追加します。

---
+++ MacroBase/MACRO/MacroBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroBase.MACRO
+{
+    public class MacroBase
+    {
+        public TMacroList parentlist;
+        public int index;
+        public bool CheckedFlg = false;
+        public string MacroParamStr = "";
+        public int CommandNo = -1;
+        public string[] Param = null;
+
+        public T GetDat<T>(int index)
+        {
+            object obj = null;
+
+            if (typeof(T) == typeof(int))
+            {
+                obj = -1;
+                if (Param.Length <= index) { return (T)obj; }
+                if (Microsoft.VisualBasic.Information.IsNumeric(Param[index]) == false) { return (T)obj; }
+                obj = Convert.ToInt32(Param[index]);
+            }
+            else if (typeof(T) == typeof(double))
+            {
+                obj = -1.0;
+                if (Param.Length <= index) { return (T)obj; }
+                if (Microsoft.VisualBasic.Information.IsNumeric(Param[index]) == false) { return (T)obj; }
+                obj = Convert.ToDouble(Param[index]);
+            }
+            else if (typeof(T) == typeof(string))
+            {
+                obj = "";
+                if (Param.Length <= index) { return (T)obj; }
+                obj = Param[index].ToString();
+            }
+
+            return (T)obj;
+        }
+    }
+
+    public class TMacroList : List<MacroBase>
+    {
+
+    }
+}
